6eb8dbe3eb41c560150a11b4d3be918a964f6f25
EricNiyo73 <ericniyokwizerwa1@gmail.com>
2022-12-24 09:53:45 +0200

    updated now

--- togglejs.js
+++
@@ -1,6 +0,0 @@
-const togglebutton = document.getElementsByClassName('toggle-button')[0]
-const navbarlinks= document.getElementsByClassName('leftside')[0]
-
-togglebutton.addEventListener('click',() =>{
-    navbarlinks.classList.toggle('active')
-})
